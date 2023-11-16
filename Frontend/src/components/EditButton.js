bd7b54df17e137684aa1e7cb3f8a5aae19bd7f6e
Laurana Traulle <lauranatraulle34@gmail.com>
2023-11-16 14:51:24 +0100

    ajout fonctionnalite editusername

--- Frontend/src/components/EditButton.js
+++ Frontend/src/components/EditButton.js
@@ -1,99 +1,92 @@
-
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateUserProfile } from '../features/updateUserName';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUserProfile } from "../features/updateUserName";
+import { setUpdateUserName } from "../features/profileSlice";
 
 
 export default function EditButton() {
-    const token = useSelector((state) => state.auth.token);
-    const profile = useSelector((state) => state.profile);
-    const firstName = useSelector((state) => state.profile.firstName);
-    const lastName = useSelector((state) => state.profile.lastName);
-    const [isEditing, setIsEditing] = useState(false);
-    const [error, setError] = useState('');
-    const [newUserName, setNewUserName] = useState(profile.userName);
-    const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const profile = useSelector((state) => state.profile);
+  const firstName = useSelector((state) => state.profile.firstName);
+  const lastName = useSelector((state) => state.profile.lastName);
+  const [isEditing, setIsEditing] = useState(false);
+  const [error, setError] = useState("");
+  const [newUserName, setNewUserName] = useState(profile.userName);
+  const dispatch = useDispatch();
 
-    const handleEdit = async (e) => {
-        e.preventDefault();
-        if (!newUserName) {
-            setError(newUserName ? '' : 'Le champ ne peut pas être vide');
-            return;
-        }
-
-        try {
-            const response = await fetch(
-                "http://localhost:3001/api/v1/user/profile",
-                {
-                  method: "PUT",
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    userName: newUserName,
-                  }),
-                }       
-            );
-
-            if (response.status === 200) {
-                const responseData = await response.json();
-                console.log(responseData)
-                dispatch(updateUserProfile({ response: responseData}));
-                setIsEditing(false);
-            } else {
-                console.error("Erreur de l'envoie du nouveau nom d'utilisateur");
-            }
-        } catch (error) {
-            console.error(
-                "Erreur de requête",
-                error
-              );
-        };
+  const handleEdit = async (e) => {
+    e.preventDefault()
+    if (!newUserName) {
+      setError("Le champ ne peut pas être vide");
+      return;
     }
-    
-    return ( 
-        <section className='edit-content'>
-            {isEditing ? (
-                <form onSubmit={handleEdit}>
-                    <div className='input-wrapper'>
-                        <label htmlFor="userName">User Name</label>
-                        <input type="text"
-                        value={newUserName}
-                        onChange={(e) => setNewUserName(e.target.value)}
-                        />
-                    </div>
-                    <div className=''>
-                        <label htmlFor="firstName">First Name</label>
-                        <input disabled type="text" 
-                        value={firstName}
-                        readOnly
-                        />
-                    </div>
-                    <div className=''>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input disabled type="text" 
-                        value={lastName}
-                        readOnly
-                        />
-                        <button type="submit">Save</button>
-                    </div>
-                        </form>
-            ) : (
 
-                    <div className='form-button'>
-                        <button className='' onClick={() => setIsEditing(true)}>
-                            Edit User Name 
-                        </button>
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/v1/user/profile",
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userName: newUserName,
+          }),
+        }
+      );
 
-                    </div>
-            )}
-        </section>
+      if (response.status === 200) {
+        dispatch(setUpdateUserName(newUserName));
+        setIsEditing(false);
+      } else {
+        console.error("Erreur de l'envoie du nouveau nom d'utilisateur");
+      }
+    } catch (error) {
+      console.error("Erreur de requête", error);
+    }
+  };
 
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+  };
 
-    )
-};
-
-
-
-
+  return (
+    <section className="edit-content">
+      {isEditing ? (
+        <form onSubmit={handleEdit}>
+          <div className="input-wrapper">
+            <label htmlFor="userName">User Name</label>
+            <input
+              type="text"
+              value={newUserName}
+              onChange={(e) => setNewUserName(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="firstName">First Name</label>
+            <input disabled type="text" value={firstName} readOnly />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="lastName">Last Name</label>
+            <input disabled type="text" value={lastName} readOnly />
+          </div>
+          <div className="edit-buttons">
+            <button className="buttons">
+              Save
+            </button>
+            <button className="buttons" onClick={handleCancelEdit}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="form-button">
+          <button className="button-eum" onClick={() => setIsEditing(true)}>
+            Edit User Name
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,53 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../assets/argentBankLogo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/argentBankLogo.png";
 import { useSelector, useDispatch } from "react-redux";
-import { setSignOut } from '../features/userSlice';
-import User from '../assets/user.webp'
-import { updateUserProfile } from "../features/updateUserName"
-
-
-
+import { setSignOut } from "../features/userSlice";
+import User from "../assets/user.webp";
+import { updateUserProfile } from "../features/updateUserName";
 
 const Header = () => {
-  const token  = useSelector((state) => state.auth.token);
-  const dataUser  = useSelector((state) => state.profile);
+  const token = useSelector((state) => state.auth.token);
+  const dataUser = useSelector((state) => state.profile);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
     localStorage.clear();
-    dispatch(setSignOut({token}));
+    dispatch(setSignOut({ token }));
   };
 
-  console.log(token, dataUser)
-    return (
-        <header>
-        <nav className='cont-nav'>
-           <Link to="/">
-              <img className='logo-header' alt='Logo de Argent Bank' src={Logo} />
-           </Link>
-
-           {token && (
-        <Link className="main-nav-item" to="/profile">
-          <img src={User} alt="User" />
-          {dataUser.userName}
+  console.log(token, dataUser);
+  return (
+    <header>
+      <nav className="cont-nav">
+        <Link to="/">
+          <img className="logo-header" alt="Logo de Argent Bank" src={Logo} />
         </Link>
-      )}
-      <Link
-        className="main-nav-item"
-        to={token ? "/" : "/signin"}
-        onClick={() => {
-          if (token) {
-            dispatch(updateUserProfile())
-            handleLogout()
-          }
-        }}>
-        {token ? "Sign Out" : "Sign In"}
-      </Link>
-        </nav>
-     </header>
-  )
-}
 
+        {token && (
+          <Link className="sign-nav" to="/profile">
+            <img src={User} alt="User" />
+            {dataUser.userName}
+          </Link>
+        )}
+        <Link
+          className="sign-nav"
+          to={token ? "/" : "/signin"}
+          onClick={() => {
+            if (token) {
+              dispatch(updateUserProfile());
+              handleLogout();
+            }
+          }}
+        >
+          {token ? "Sign Out" : "Sign In"}
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
